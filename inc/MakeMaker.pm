f706cf2c5f0d5029fe93b1bf03e7f4817a237d23
Alessandro Ghedini <al3xbio@gmail.com>
2012-08-21 16:26:52 +0200

    MakeMaker.pm: link to openssl

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -9,6 +9,8 @@ override _build_MakeFile_PL_template => sub {
 
 	my $template  = <<'EOS';
 use File::Which;
+use Devel::CheckLib;
+check_lib_or_exit(lib => 'ssl');
 
 unless (which("cmake")) { print "Can't find cmake\n"; exit }
 
@@ -29,6 +31,7 @@ EOS
 override _build_WriteMakefile_args => sub {
 	return +{
 		%{ super() },
+		LIBS	=> '-lssl',
 		INC	=> '-I. -Ixs/libgit2/include',
 		OBJECT	=> '$(O_FILES) xs/libgit2.a',
 	}
