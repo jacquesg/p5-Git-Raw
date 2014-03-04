b8bf3e1eae5bdc2965db1e46be635de4cdb2920d
Jacques Germishuys <jacquesg@striata.com>
2014-03-04 12:33:10 +0200

    Stash.pm: Updated documentation for save() method

--- lib/Git/Raw/Stash.pm
+++ lib/Git/Raw/Stash.pm
@@ -18,9 +18,31 @@ B<WARNING>: The API of this module is unstable and may change without warning
 
 =head1 METHODS
 
-=head2 save( $repo, $stasher, $msg )
+=head2 save( $repo, $stasher, $msg [, \@opts] )
 
-Save the local modifications to a new stash.
+Save the local modifications to a new stash. Non-default options may be
+specified by providing the optional C<@opts> argument.
+
+Valid fields for the C<@opts> array are:
+
+=over 4
+
+=item * "keep_index"
+
+All changes already added to the index are left intact in the working directory.
+The default is to also stash changes in the index.
+
+=item * "include_untracked"
+
+All untracked files are also stashed and then cleaned up from the working
+directory. The default is to leave untracked files in the working directory.
+
+=item * "include_ignored"
+
+All ignored files are also stashed and then cleaned up up from the working
+directory. The default is to leave ignored files in the working directory.
+
+=back
 
 =head2 foreach( $repo, $callback )
 
